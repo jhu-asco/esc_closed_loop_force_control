569f45609cf4824207d1aefc9b823ec035855939
jchung55 <jchung55@jhu.edu>
2017-07-12 17:58:24 -0400

    Added/Fixed comments

--- arduino_communication.py
+++ arduino_communication.py
@@ -2,29 +2,37 @@ import serial
 import time
 import struct
 
+# Wrapper class to send PWM percentages and recieve battery voltages through Arduino
 class ArduinoCommunication:
     
-    def __init__(self):
+    # Create Constructor
+    # Timeout: time for arduino to communicate with computer
+    def __init__(self, port = 'com6', baud_rate = 9600, timeout = 2):
+        # Scaling data value from analog input (1023) to voltage (5 V) with voltage sensor scaling (10.1)        
         self.voltage_scale = (5.0/1023.0)*10.1
-        self.port = 'com6'
-        self.baud_rate = 9600
-        self.connectingWithArduino()
-    
-    def connectingWithArduino(self, timeout = 2):
-        self.arduino = serial.Serial(self.port, self.baud_rate)
+        # Connects with arduino
+        self.arduino = serial.Serial(port, baud_rate)
         time.sleep(timeout)
     
-    def sendMotorCommandtoArduino(self, percentage = 0):
+    # Sends PWM percentages to the Arduino
+    def sendMotorCommandToArduino(self, percentage = 0):
+        # Packs the data as a big-endian integer and writes to the arduino
         self.arduino.write(struct.pack('>B', percentage))
         
-    def getVoltageFromArduino(self):    
+    # Gets converted battery voltage values from the Arduino    
+    def getVoltageFromArduino(self):
+        # Reads and stores data from the arduino
         self.data = self.arduino.read(2)
+        # if the data is not gibberish 
         if self.data:
+            # unpacks the data from the arduino as a little-endian integer and stores into variable
             adc_output = struct.unpack('<H', self.data)
+            # Converts data value to actual voltage 
             voltage = adc_output[0]*self.voltage_scale
             return voltage
         return None
         
+    # Closes the arduino connection
     def closeArduino(self):
         self.arduino.close()
     
--- test_arduino_commmunication.py
+++ test_arduino_commmunication.py
@@ -1,13 +1,23 @@
+#Test for the Ardufly/pilot/Mega: Sending PWM signals and recieving battery voltage values
+# Written by: Joseph Chung under ASCOL
+# Email: jchung55@jhu.edu
+# Date: 7/11/17
+
 from arduino_communication import ArduinoCommunication
 import numpy as np
 import time
 
-percentage_array = np.linspace(6,55, 5)
+# Create a percentage array of motor values
+percentage_array = np.linspace(6, 55, 5)
+# Instantiate the class into main
 arduino_communication = ArduinoCommunication()
+# Send motor commands to the arduino and recieve voltage values 
 for i in range(0, len(percentage_array), 1):
     percent = percentage_array[i]
-    arduino_communication.sendMotorCommandtoArduino(percent)
+    arduino_communication.sendMotorCommandToArduino(percent)
     time.sleep(2)
     print(arduino_communication.getVoltageFromArduino())
-arduino_communication.sendMotorCommandtoArduino(0)
+# Reset the motor to zero (0) after all trials are done
+arduino_communication.sendMotorCommandToArduino(0)
+# Close the arduino
 arduino_communication.closeArduino()
